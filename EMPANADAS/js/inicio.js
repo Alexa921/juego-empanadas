694d0a2f5e6a95355b2f1b2be68f856c2c9fbbea
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2026-08-18 21:39:32 -0500

    primer avance

---
+++ EMPANADAS/js/inicio.js
@@ -0,0 +1,7 @@
+const botonJugar = document.getElementById("btn-jugar");
+
+botonJugar.addEventListener("click", function () {
+
+    window.location.href = "niveles.html";
+
+});
---
+++ EMPANADAS/js/niveles.js
@@ -0,0 +1,22 @@
+const nivel1 = document.getElementById("nivel-1");
+const botonVolver = document.getElementById("btn-volver");
+
+
+nivel1.addEventListener("click", function () {
+
+    window.location.href = "juego.html";
+
+});
+
+
+botonVolver.addEventListener("click", function () {
+
+    window.location.href = "index.html";
+
+});
+
+nivel1.addEventListener("click", function () {
+
+    window.location.href = "juego.html";
+
+});
